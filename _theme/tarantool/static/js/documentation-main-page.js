50de0493338fc6dfae91ebd84eadd66d6d994e78
ogogorev <rednowall@mail.ru>
2017-08-02 23:13:33 +0600

    redesign docs main page

--- _theme/tarantool/static/js/documentation-main-page.js
+++ _theme/tarantool/static/js/documentation-main-page.js
@@ -1,32 +1,40 @@
 $(function() {
-    var doc_main_page_title = '.documentation-main-page-title';
-    var b_block_wrapper = '.b-block-wrapper';
-    var b_section_title = '.b-section-title';
-    var nav_button = "#start-menu-nav-button";
-    var doc_main_page_content = '.documentation-main-page-content';
-    var start_menu = '.start-menu';
+    // var doc_main_page_title = '.documentation-main-page-title';
+    // var b_block_wrapper = '.b-block-wrapper';
+    // var b_section_title = '.b-section-title';
+    // var nav_button = "#start-menu-nav-button";
+    // var start_menu = '.start-menu';
+    var doc_main_page_header_path = '.documentation-main-page-header-path';
+    var version_switcher = '.version-switcher';
 
-    if ($(doc_main_page_title)) {
-      $(doc_main_page_title).find(b_section_title).addClass('toggle-navigation');
-      $(doc_main_page_title).find(b_section_title).attr('id', 'start-menu-nav-button');
+    // Adding available versions list to doc main page
+    if ($(doc_main_page_header_path)) {
+      if ($(version_switcher)) {
+        $(doc_main_page_header_path).after($(version_switcher));
+      }
     }
 
-    $(nav_button).on('click', function(event) {
-        event.stopPropagation();
-        if (!$(this).hasClass('active')) {
-            $(this).addClass('active');
-            $(start_menu).addClass('active');
-            $("body").addClass('stop-scroll');
-        } else {
-            $(this).removeClass('active');
-            $(start_menu).removeClass('active');
-            $("body").removeClass('stop-scroll');
-        }
-    });
+    // if ($(doc_main_page_title)) {
+    //   $(doc_main_page_title).find(b_section_title).addClass('toggle-navigation');
+    //   $(doc_main_page_title).find(b_section_title).attr('id', 'start-menu-nav-button');
+    // }
 
-    $("body").click(function() {
-        $(start_menu).removeClass("active");
-        $(nav_button).removeClass("active");
-        $("body").removeClass('stop-scroll');
-    });
+    // $(nav_button).on('click', function(event) {
+    //     event.stopPropagation();
+    //     if (!$(this).hasClass('active')) {
+    //         $(this).addClass('active');
+    //         $(start_menu).addClass('active');
+    //         $("body").addClass('stop-scroll');
+    //     } else {
+    //         $(this).removeClass('active');
+    //         $(start_menu).removeClass('active');
+    //         $("body").removeClass('stop-scroll');
+    //     }
+    // });
+
+    // $("body").click(function() {
+    //     $(start_menu).removeClass("active");
+    //     $(nav_button).removeClass("active");
+    //     $("body").removeClass('stop-scroll');
+    // });
 });
--- doc/1.6/index.rst
+++ doc/1.6/index.rst
@@ -6,28 +6,26 @@
 -------------------------------------------------------------------------------
 
 .. wp_section::
-    :title: Tarantool 1.6 manual
-    :class: b-block-lightgray b-downloads-top documentation-main-page-title
+    :class: documentation-main-page-header
+
+    .. container:: documentation-main-page-header-path
+
+        :doc:`Documentation main <index>` /
 
 .. wp_section::
-    :class: b-block b-documentation-toc
+    :class: b-documentation-toc
 
     .. container:: documentation-main-page
 
-        .. container:: start-menu
+        .. container:: documentation-main-page-title
 
-            - Manual in other formats
-                - `Single page <singlehtml.html>`_
-                - `PDF <Tarantool.pdf>`_
-            - Manual for other versions
-                - `Tarantool 1.8 <../1.8/index.html>`_
-                - `Tarantool 1.7 <../1.7/index.html>`_
-                - `Tarantool 1.6 <../1.6/index.html>`_
-            - Other sources
-                - Discussion boards
-                    - `English`_
-                    - `Russian`_
-                - `Telegram chat`_
+            Tarantool 1.6 manual
+
+        .. container:: documentation-main-page-description
+
+            This manual embraces all aspects of using Tarantool: from introductory
+            information and exercises for beginners -- to advanced instructions and
+            detailed references for power users and contributors.
 
         .. container:: documentation-main-page-content
 
@@ -38,17 +36,21 @@
                     Документация находится в процессе перевода и может
                     отставать от английской версии.
 
-            .. wp_section::
-
-                This manual embraces all aspects of using Tarantool: from introductory
-                information and exercises for beginners -- to advanced instructions and
-                detailed references for power users and contributors.
-
-            .. wp_section::
-                :title: Parts of the documentation:
-                :class: doc-parts-title
-
             .. include:: singlehtml.rst
+
+        .. container:: other-sources-menu
+
+            .. raw:: html
+
+                <div class="badge-icon"></div>
+
+            Other sources
+
+            - `Single page <singlehtml.html>`_
+            - `PDF <Tarantool.pdf>`_
+            - `English`_
+            - `Russian`_
+            - `Telegram chat`_
 
 .. _English: https://groups.google.com/forum/#!forum/tarantool
 .. _Russian: https://googlegroups.com/group/tarantool-ru
