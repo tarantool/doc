841cdd0bfca5b4b6451593ffbddd73c3697ccbf2
Alexander Turenko <alexander.turenko@tarantool.org>
2018-05-31 19:37:16 +0300

    Revert "Add trigger for gl in travis builds (#458)"
    This reverts commit 188b6860ba5291d0cd19e539ea18304e72d9f12a.

--- trigger.sh
+++
@@ -1,4 +0,0 @@
-echo -en 'Trigger';
-for i in {1..30}}; do sleep 1s; echo -en '.'; done
-curl -X POST -F token=$1 -F ref=master https://gitlab.com/api/v4/projects/4657341/trigger/pipeline
-echo 'Done';
