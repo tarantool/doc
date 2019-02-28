a8b3335964410dfe2692f88e60c6d8f19d342923
pgulutzan <pgulutzan@ocelot.ca>
2019-02-28 13:52:13 -0700

    Changed version to 2.1.1

--- conf.py
+++ conf.py
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 project = u'Tarantool'
 
 # |release| The full version, including alpha/beta/rc tags.
-release = "2.1.0"
+release = "2.1.1"
 # |version| The short X.Y version.
 version = '.'.join(release.split('.')[0:2])
 
