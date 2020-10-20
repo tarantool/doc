2b2ae625ba76be88f82cd2900d41ea9a404d47d1
Natalia Ogoreltseva <n.ogoreltseva@corp.mail.ru>
2020-10-20 13:37:10 +0300

    gh-1580 [x] Add reference for luatest and cartridge (just a link)

--- update_submodules.sh
+++ update_submodules.sh
@@ -23,6 +23,13 @@ cartridge_cli_dest="${rst_dest}/cartridge_cli"
 cartridge_cli_index_dest="${cartridge_cli_dest}/index.rst"
 monitoring_root="${project_root}/modules/metrics/doc/monitoring"
 monitoring_dest="${project_root}/doc/book"
+luatest_root="${project_root}/modules/luatest/"
+luatest_dest="${project_root}/doc/reference/reference_rock/luatest"
+
+cd "${luatest_root}"
+ldoc --ext=rst --dir=rst --toctree="API" .
+cd "${luatest_dest}"
+yes | cp -fa "${luatest_root}/rst/." "${luatest_dest}"
 
 mkdir -p "${monitoring_dest}"
 yes | cp -rf "${monitoring_root}" "${monitoring_dest}/"
@@ -31,11 +38,7 @@ mkdir -p "${cartridge_cli_dest}"
 yes | cp -rf "${cartridge_cli_root}/README.rst" "${cartridge_cli_index_dest}"
 
 cd "${cartridge_root}" || exit
-tarantoolctl rocks install \
-  https://raw.githubusercontent.com/tarantool/LDoc/tarantool/ldoc-scm-2.rockspec \
-  --server=http://rocks.moonscript.org
 CMAKE_DUMMY_WEBUI=true tarantoolctl rocks make
-
 
 cd "${rst_src}" || exit
 mkdir -p "${rst_dest}"
