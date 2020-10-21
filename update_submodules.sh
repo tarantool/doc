62e67ce321700e0684f0b1bc3d17622b540c7ff5
Natalia Ogoreltseva <37013254+Onvember@users.noreply.github.com>
2020-10-21 14:30:15 +0300

    gh-1580 add Luatest documentation, remove tbd and shard ref from index, add cartridge doc to ref rocks

--- update_submodules.sh
+++ update_submodules.sh
@@ -23,6 +23,14 @@ cartridge_cli_dest="${rst_dest}/cartridge_cli"
 cartridge_cli_index_dest="${cartridge_cli_dest}/index.rst"
 monitoring_root="${project_root}/modules/metrics/doc/monitoring"
 monitoring_dest="${project_root}/doc/book"
+luatest_root="${project_root}/modules/luatest/"
+luatest_dest="${project_root}/doc/reference/reference_rock/luatest"
+
+cd "${luatest_root}"
+ldoc --ext=rst --dir=rst --toctree="API" .
+cd "${luatest_dest}"
+yes | cp -fa "${luatest_root}/rst/." "${luatest_dest}"
+yes | cp "${luatest_root}/README.rst" "${luatest_dest}"
 
 mkdir -p "${monitoring_dest}"
 yes | cp -rf "${monitoring_root}" "${monitoring_dest}/"
@@ -35,7 +43,6 @@ tarantoolctl rocks install \
   https://raw.githubusercontent.com/tarantool/LDoc/tarantool/ldoc-scm-2.rockspec \
   --server=http://rocks.moonscript.org
 CMAKE_DUMMY_WEBUI=true tarantoolctl rocks make
-
 
 cd "${rst_src}" || exit
 mkdir -p "${rst_dest}"
