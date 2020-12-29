44faa5b9006dc37ba55f02769fdd26827423015e
Naidenov Ivan <i.naidenov@corp.mail.ru>
2020-12-29 15:57:13 +0300

    Add cartridge kubernetes guide
    (cherry picked from commit f163a01f11479600849e42f035e0526ea2574288)

--- update_submodules.sh
+++ update_submodules.sh
@@ -26,6 +26,9 @@ monitoring_dest="${project_root}/doc/book"
 luatest_root="${project_root}/modules/luatest/"
 luatest_dest="${project_root}/doc/reference/reference_rock/luatest"
 
+cartridge_kubernetes_root="${project_root}/modules/tarantool-operator/doc/cartridge_kubernetes_guide"
+cartridge_kubernetes_dest="${rst_dest}/"
+
 cd "${luatest_root}"
 ldoc --ext=rst --dir=rst --toctree="API" .
 cd "${luatest_dest}"
@@ -37,6 +40,9 @@ yes | cp -rf "${monitoring_root}" "${monitoring_dest}/"
 
 mkdir -p "${cartridge_cli_dest}"
 yes | cp -rf "${cartridge_cli_root}/README.rst" "${cartridge_cli_index_dest}"
+
+mkdir -p "${cartridge_kubernetes_dest}"
+yes | cp -rf "${cartridge_kubernetes_root}" "${cartridge_kubernetes_dest}"
 
 cd "${cartridge_root}" || exit
 tarantoolctl rocks install \
@@ -56,3 +62,4 @@ cd "${po_src}" || exit
 mkdir -p "${po_dest}"
 find . -name '*.po' -exec cp -r --parents {} "${po_dest}" \;
 
+sed -i -e '/Cartridge CLI<cartridge_cli\/index>/a\' -e '\ \ \ Cartridge Kubernetes guide<cartridge_kubernetes_guide/index>' "${rst_dest}/index.rst"
