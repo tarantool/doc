9c466a2ea453229d9901df1818b6d3368c0ac068
Artem Morozov <artembo@me.com>
2021-01-29 12:41:06 +0300

    Deploy the latest branch in place of 2.6
    Gitlab-ci:
    '2.6' branch was removed from handled for ci branches
    and replaced with 'latest'
    
    upload_output.sh:
    'latest' branch is treated like other branches
    + it is deployed to the $LATEST_DOC version on the website
    which is set up for 2.6 now
    
    Closes #1774

--- upload_output.sh
+++ upload_output.sh
@@ -20,23 +20,23 @@ aws s3 cp --acl public-read output/html/ru/singlehtml.html $S3_PATH/$CI_COMMIT_R
 
 curl --data '{"update_key":"'"$TARANTOOL_UPDATE_KEY"'"}' --header "Content-Type: application/json" --request POST "$TARANTOOL_UPDATE_URL""$CI_COMMIT_REF_NAME"/
 
-if [ "${LATEST_DOC}" = "${CI_COMMIT_REF_NAME}" ]; then
+if [ "${CI_COMMIT_REF_NAME}" = "latest" ]; then
   # upload json and images
-  aws s3 rm $S3_PATH/latest/json --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
-  aws s3 cp output/json $S3_PATH/latest/json --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive --include "*" --exclude "*.jpg" --exclude "*.png" --exclude "*.svg"
-  aws s3 cp output/json/_build_en/json/_images $S3_PATH/latest/images_en --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
-  aws s3 cp output/json/_build_ru/json/_images $S3_PATH/latest/images_ru --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
+  aws s3 rm $S3_PATH/$LATEST_DOC/json --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
+  aws s3 cp output/json $S3_PATH/$LATEST_DOC/json --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive --include "*" --exclude "*.jpg" --exclude "*.png" --exclude "*.svg"
+  aws s3 cp output/json/_build_en/json/_images $S3_PATH/$LATEST_DOC/images_en --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
+  aws s3 cp output/json/_build_ru/json/_images $S3_PATH/$LATEST_DOC/images_ru --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
 
   # upload pdf files
-  aws s3 cp --acl public-read output/_latex_en/Tarantool.pdf $S3_PATH/latest/Tarantool-en.pdf --endpoint-url=$AWS_S3_ENDPOINT_URL
-  aws s3 cp --acl public-read output/_latex_ru/Tarantool.pdf $S3_PATH/latest/Tarantool-ru.pdf --endpoint-url=$AWS_S3_ENDPOINT_URL
+  aws s3 cp --acl public-read output/_latex_en/Tarantool.pdf $S3_PATH/$LATEST_DOC/Tarantool-en.pdf --endpoint-url=$AWS_S3_ENDPOINT_URL
+  aws s3 cp --acl public-read output/_latex_ru/Tarantool.pdf $S3_PATH/$LATEST_DOC/Tarantool-ru.pdf --endpoint-url=$AWS_S3_ENDPOINT_URL
 
   # upload singlehtml and assets
-  aws s3 cp --acl public-read output/html/en/_static $S3_PATH/latest/en/_static --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
-  aws s3 cp --acl public-read output/html/en/_images $S3_PATH/latest/en/_images --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
-  aws s3 cp --acl public-read output/html/en/singlehtml.html $S3_PATH/latest/en/singlehtml.html --endpoint-url=$AWS_S3_ENDPOINT_URL
-  aws s3 cp --acl public-read output/html/ru/_static $S3_PATH/latest/ru/_static --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
-  aws s3 cp --acl public-read output/html/ru/_images $S3_PATH/latest/ru/_images --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
-  aws s3 cp --acl public-read output/html/ru/singlehtml.html $S3_PATH/latest/ru/singlehtml.html --endpoint-url=$AWS_S3_ENDPOINT_URL
-  curl --data '{"update_key":"'"$TARANTOOL_UPDATE_KEY"'"}' --header "Content-Type: application/json" --request POST "$TARANTOOL_UPDATE_URL"latest/
+  aws s3 cp --acl public-read output/html/en/_static $S3_PATH/$LATEST_DOC/en/_static --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
+  aws s3 cp --acl public-read output/html/en/_images $S3_PATH/$LATEST_DOC/en/_images --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
+  aws s3 cp --acl public-read output/html/en/singlehtml.html $S3_PATH/$LATEST_DOC/en/singlehtml.html --endpoint-url=$AWS_S3_ENDPOINT_URL
+  aws s3 cp --acl public-read output/html/ru/_static $S3_PATH/$LATEST_DOC/ru/_static --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
+  aws s3 cp --acl public-read output/html/ru/_images $S3_PATH/$LATEST_DOC/ru/_images --endpoint-url=$AWS_S3_ENDPOINT_URL --recursive
+  aws s3 cp --acl public-read output/html/ru/singlehtml.html $S3_PATH/$LATEST_DOC/ru/singlehtml.html --endpoint-url=$AWS_S3_ENDPOINT_URL
+  curl --data '{"update_key":"'"$TARANTOOL_UPDATE_KEY"'"}' --header "Content-Type: application/json" --request POST "$TARANTOOL_UPDATE_URL""$LATEST_DOC"/
 fi
