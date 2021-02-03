d42a8060a41f4ba37a1ee109b9af3919406226d7
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2021-02-03 15:25:57 +0300

    Add Grafana dashboard documentation on tarantool.io
    Closes https://github.com/tarantool/grafana-dashboard/issues/45 together with https://github.com/tarantool/metrics/pull/185 and https://github.com/tarantool/grafana-dashboard/pull/50

--- update_submodules.sh
+++ update_submodules.sh
@@ -23,6 +23,7 @@ cartridge_cli_dest="${rst_dest}/cartridge_cli"
 cartridge_cli_index_dest="${cartridge_cli_dest}/index.rst"
 monitoring_root="${project_root}/modules/metrics/doc/monitoring"
 monitoring_dest="${project_root}/doc/book"
+monitoring_grafana_root="${project_root}/modules/grafana-dashboard/doc/monitoring"
 luatest_root="${project_root}/modules/luatest/"
 luatest_dest="${project_root}/doc/reference/reference_rock/luatest"
 
@@ -37,6 +38,7 @@ yes | cp "${luatest_root}/README.rst" "${luatest_dest}"
 
 mkdir -p "${monitoring_dest}"
 yes | cp -rf "${monitoring_root}" "${monitoring_dest}/"
+yes | cp -rf "${monitoring_grafana_root}" "${monitoring_dest}/"
 
 mkdir -p "${cartridge_cli_dest}"
 yes | cp -rf "${cartridge_cli_root}/README.rst" "${cartridge_cli_index_dest}"
