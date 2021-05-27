7de13e68ff126079e97cb22ac78cb855c38b6751
Artem Morozov <artembo@me.com>
2021-05-27 14:26:55 +0300

    Add deploy-custom workflow replace pull_submodules.sh with pull_submodules.py update pull-modules make command moved init submodules command in workflow
    Co-authored-by: Nick Volynkin <nick.volynkin@gmail.com>

--- pull_submodules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-mkdir -p ~/.ssh/
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-git submodule update --init
-git fetch --recurse-submodules
-git submodule update --remote --checkout
