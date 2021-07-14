e07096acba123f83b03e4759049f6af9cf007b29
Artem Morozov <artembo@me.com>
2021-07-14 15:12:15 +0300

    Add pipelines to push sources and pull translations from Crowdin (#2229)
    - add cleanup.py for re-arranging po files and make correspondind changes in CMakeLists.txt
    - push translation for pushing sources to crowdin on pull request when rst is changed
    - add pull-translation.yml for getting translations from crowdin on demand when translation is ready
    - copy pull_submodules.py from latest branch for consistency
    
    Closes #2208

---
+++ pull_submodules.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python3
+
+import os
+import subprocess
+
+
+modules_dir = 'modules'
+modules = {
+    'cartridge': 'INPUT_CARTRIDGE',
+    'cartridge-cli': 'INPUT_CARTRIDGE_CLI',
+    # 'grafana-dashboard': 'INPUT_GRAFANA',
+    'luatest': 'INPUT_LUATEST',
+    'metrics': 'INPUT_METRICS',
+    'tarantool-operator': 'INPUT_K8S_OPERATOR',
+    # 'tntcxx': 'INPUT_CPP_DRIVER',
+}
+workdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'modules')
+
+
+def main():
+    """
+    set -x
+
+    mkdir -p ~/.ssh/
+    ssh-keyscan github.com >> ~/.ssh/known_hosts
+    git submodule update --init
+    git fetch --recurse-submodules
+    git submodule update --remote --checkout
+    """
+
+    in_ci = os.environ.get("CI")
+    if not in_ci:
+        mkdir_p("~/.ssh/")
+        commands = "set -ex ;" \
+                   "ssh-keyscan github.com >> ~/.ssh/known_hosts ;" \
+                   "git submodule update --init ;" \
+                   "git fetch --recurse-submodules"
+
+        subprocess.check_call(commands, shell=True)
+
+    for directory, git_ref_variable in modules.items():
+        git_ref = os.environ.get(git_ref_variable, 'origin/master')
+        checkout_submodule(directory, git_ref)
+
+
+def checkout_submodule(directory, git_ref='origin/master'):
+    print(f'Checking out {directory} module into {git_ref}')
+    path = os.path.join(workdir, directory)
+    print(path)
+    subprocess.check_call('set -ex ; git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"', shell=True,
+                          cwd=path)
+    git_checkout = f'set -ex ; git remote update ; git checkout {git_ref}'
+    subprocess.check_call(git_checkout, shell=True,
+                          cwd=path)
+
+
+def mkdir_p(path):
+    """
+    Creates a directory with subdirectories, just like `mkdir -p path`
+    """
+    import pathlib
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+
+if __name__ == "__main__":
+    main()
