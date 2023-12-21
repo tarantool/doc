1d6dd6c25116aaa09de1c908a91f769aafcfb488
Kseniia Antonova <73473519+xuniq@users.noreply.github.com>
2023-12-22 00:12:33 +0300

    Use a new configuration approach for bootstrapping a database in SQL tutorials (#3891)
    * Update Improving MySQL with Tarantool page
    * Updated SQL tutorial page
    
    Fixes #3672

---
+++ doc/code_snippets/snippets/config/instances.enabled/mysql/myapp.lua
@@ -0,0 +1,17 @@
+-- myapp.lua --
+
+box.schema.user.grant('guest', 'read,write,execute', 'universe')
+
+local function bootstrap()
+    if not box.space.mysqldaemon then
+        s = box.schema.space.create('mysqldaemon')
+        s:create_index('primary',
+                { type = 'tree', parts = { 1, 'unsigned' }, if_not_exists = true })
+    end
+    if not box.space.mysqldata then
+        t = box.schema.space.create('mysqldata')
+        t:create_index('primary',
+                { type = 'tree', parts = { 1, 'unsigned' }, if_not_exists = true })
+    end
+end
+bootstrap()
