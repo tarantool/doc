469ec7b77fb3114b83619adc1962b92f93f9df5d
andreyaksenov <duuusha@gmail.com>
2024-05-23 12:50:22 +0300

    Applications: use new roles in the 'crud' sample

--- doc/code_snippets/snippets/sharding/instances.enabled/sharded_cluster_crud/router.lua
+++ doc/code_snippets/snippets/sharding/instances.enabled/sharded_cluster_crud/router.lua
@@ -1,17 +1,1 @@
 local vshard = require('vshard')
-local crud = require('crud')
-
-local M = {}
-
-function M.validate()
-end
-
-function M.apply()
-    crud.init_router()
-end
-
-function M.stop()
-    crud.stop_router()
-end
-
-return M
