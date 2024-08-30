e4df6d8c54783a9c5fe4a91ec83e1b5e65c5d1e3
andreyaksenov <duuusha@gmail.com>
2024-08-30 15:06:52 +0300

    Schema validation: samples - formatting/naming/cfg

--- doc/code_snippets/snippets/config/instances.enabled/application_validate_cfg_record_methods/http_api.lua
+++ doc/code_snippets/snippets/config/instances.enabled/application_validate_cfg_record_methods/http_api.lua
@@ -2,25 +2,30 @@
 local log = require('log').new("http_api")
 local schema = require('experimental.config.utils.schema')
 
-local listen_address_schema = schema.new('listen_address', schema.record({
-    scheme = schema.enum({ 'http', 'https' }),
-    host = schema.scalar({ type = 'string' }),
-    port = schema.scalar({ type = 'integer' })
-}), { methods = {
-    get_url_string = function(_self, url)
-        return string.format("%s://%s:%d", url.scheme, url.host, url.port)
-    end,
-} })
+local listen_address_schema = schema.new(
+        "listen_address",
+        schema.record(
+                {
+                    scheme = schema.enum({ "http", "https" }),
+                    host = schema.scalar({ type = "string" }),
+                    port = schema.scalar({ type = "integer" })
+                }
+        ),
+        {
+            methods = {
+                format = function(_self, url)
+                    return string.format("%s://%s:%d", url.scheme, url.host, url.port)
+                end
+            }
+        }
+)
 
 local function validate(cfg)
     listen_address_schema:validate(cfg)
 end
 
 local function apply(cfg)
-    local scheme = listen_address_schema:get(cfg, 'scheme')
-    local host = listen_address_schema:get(cfg, 'host')
-    local port = listen_address_schema:get(cfg, 'port')
-    local url_string = listen_address_schema:get_url_string({ scheme = scheme, host = host, port = port })
+    local url_string = listen_address_schema:format(cfg)
     log.info("HTTP API endpoint: %s", url_string)
 end
 
